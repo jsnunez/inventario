3511be083e138f5b37e0a79b9149fb02806097f6
joan sebastian nuñez serrano <jsnunez94@gmail.com>
2025-02-22 22:59:23 -0500

    railway

--- config/database.js
+++ config/database.js
@@ -1,8 +1,13 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('ferreteriain3', 'root', '12345', {
-  host: 'localhost',
+// const sequelize = new Sequelize('ferreteriain3', 'root', '12345', {
+//   host: 'localhost',
+//   dialect: 'mysql',
+// });
+const sequelize = new Sequelize('railway', 'root', 'YtLDWMqKeNmrWreijmOiTsnYuORyfPQg', {
+  host: 'trolley.proxy.rlwy.net',
   dialect: 'mysql',
+  port: 35017,
 });
 
 module.exports = sequelize;
